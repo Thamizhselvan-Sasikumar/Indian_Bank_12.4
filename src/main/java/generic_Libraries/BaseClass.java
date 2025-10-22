e87eac3ef261aa11acdf3c2f3cf4b9815226eb53
Thamizhselvan-Sasikumar <thamizhleo24@gmail.com>
2025-10-22 17:52:39 +0530

    Worked Browser setting and Handled SSL Cwerificate

--- src/main/java/generic_Libraries/BaseClass.java
+++ src/main/java/generic_Libraries/BaseClass.java
@@ -2,41 +2,59 @@ package generic_Libraries;
 
 import org.testng.annotations.BeforeMethod;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import pom_package.BaseClassPage;
 
 import org.testng.annotations.BeforeClass;
+
+import java.io.IOException;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseClass {
 
 	public WebDriver d;
 	
-	
-
 	@BeforeClass
-	public void launchBrowser() throws InterruptedException {
+	public void launchBrowser() throws InterruptedException, IOException {
 		
+		String browser = UtilityMethod.getProperty("Browser");
 		
+		if(browser.equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+	        ChromeOptions op = new ChromeOptions();
+	        op.setAcceptInsecureCerts(true);
+			d = new ChromeDriver(op);
+		}
+		else if(browser.equalsIgnoreCase("Edge")) {
+			d = new EdgeDriver();
+		}
+		else if (browser.equalsIgnoreCase("FireFox")) {
+			d = new FirefoxDriver();
+		}
 		
-		d = new ChromeDriver();
 		d.manage().window().maximize();
-		d.get("https://192.9.200.27:2322/IB_12_4/login.htm");
+		d.get(UtilityMethod.getProperty("URL"));
 		
-		BaseClassPage bccp = new BaseClassPage(d);
-		bccp.getDetailButton().click();
-		bccp.getProceedLink().click();
+		/*
+		 * BaseClassPage bccp = new BaseClassPage(d); bccp.getDetailButton().click();
+		 * bccp.getProceedLink().click();
+		 */
 
 	}
 
 	@BeforeMethod
-	public void loginMethod() throws InterruptedException {
+	public void loginMethod() throws InterruptedException, IOException {
 
 		BaseClassPage bccp = new BaseClassPage(d);
-		bccp.getUserID().sendKeys("NCSTS");
-		bccp.getPassword().sendKeys("1234");
+		bccp.getUserID().sendKeys(UtilityMethod.getProperty("UN"));
+		bccp.getPassword().sendKeys(UtilityMethod.getProperty("PWD"));
 		bccp.getLoginButton().click();
 		Thread.sleep(2000);
 		try {
